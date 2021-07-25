8cf123621d7a7b2badb008b2f6b3dc1fe9ed17fe
Romaric <romaric@MacBook-Pro-2.local>
2021-07-25 16:34:14 +0200

    setup webcam and canvas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from "react-webcam";
+import { useRef } from "react";
 
 function App() {
+  // setup refencies
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const innerStyle = {
+    width: 640,
+    height: 480,
+    backgroundColor: "black",
+    position: "relative",
+  };
+  const canvasStyle = {
+    position: "absolute",
+    zIndex: "1",
+    top: "0",
+    left: "0",
+    right: "0",
+    bottom: "0",
+    width: 640,
+    height: 480,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inner" style={innerStyle}>
+        <Webcam ref={webcamRef} style={canvasStyle}></Webcam>
+        <canvas ref={canvasRef} style={canvasStyle}></canvas>
+      </div>
     </div>
   );
 }
