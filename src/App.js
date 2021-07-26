561c6911ef1cc2c1cec03ec3dbbf6c95a591772e
Romaric <romaric@MacBook-Pro-2.local>
2021-07-26 17:28:53 +0200

    detect functions

--- src/App.js
+++ src/App.js
@@ -3,12 +3,46 @@ import "./App.css";
 import * as tf from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/facemesh";
 import Webcam from "react-webcam";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 
 function App() {
   // setup refencies
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+
+  // load facemesh
+  const runFacemesh = async () => {
+    const net = await facemesh.load();
+    setInterval(() => {
+      detect(net);
+    }, 10000);
+  };
+
+  // detect function
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // // Get Video Properties
+      const video = webcamRef.current.video;
+      // const videoWidth = webcamRef.current.video.videoWidth;
+      // const videoHeight = webcamRef.current.video.videoHeight;
+      // // Set video width
+      // webcamRef.current.video.width = videoWidth;
+      // webcamRef.current.video.height = videoHeight;
+      // // Set canvas width
+      // canvasRef.current.width = videoWidth;
+      // canvasRef.current.height = videoHeight;
+      const faces = await net.estimateFaces(video);
+      console.log(faces);
+    }
+  };
+
+  useEffect(() => {
+    runFacemesh();
+  });
 
   const innerStyle = {
     width: 640,
